4684e16e5dc6b9233c7e34bc492124dc687634e6
SwatiKumawat <40262006+SwatiKumawat@users.noreply.github.com>
2018-07-13 11:09:08 +0530

    Update project_1.py
    on the way of processing

--- project_1.py
+++ project_1.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python2
+
 import pytesseract
-import os
+import pyttsx
 import cv2 as cv
-from espeak import espeak
-import sys
-img = cv.imread("a.png",0)
+#import os
+#from espeak import espeak
+#import sys
+
+# reading image
+img = cv.imread("test.png",0)
 
 config = ('-l eng --oem 1 --psm 3')
-
+#converting image text to string
 text = pytesseract.image_to_string(img, config=config)
+#printing the extracted text
 print text
 
+#say the text from image
+engine=pyttsx.init()
+engine.say(text)
+engine.runAndWait()
+
+'''
 a=text.decode('utf-8')
 #a = text.split(' ')
 espeak.synth(a)
-
-
 
 li = text.split(' ')
 #print type(li)
@@ -24,4 +33,4 @@ for i in li:
 	#print i
 	#print i.decode('utf-8')
 	espeak.synth(i)
-
+'''
